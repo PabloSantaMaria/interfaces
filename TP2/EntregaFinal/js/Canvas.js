028bda0d0c108137d84e5c6080e22c087b749ab2
PabloSantaMaria <pabloj.santamaria@gmail.com>
2019-09-08 22:40:13 -0300

    drag vertex

--- TP2/EntregaFinal/js/Canvas.js
+++ TP2/EntregaFinal/js/Canvas.js
@@ -9,20 +9,98 @@ class Canvas {
     this.canvas.width = width;
     this.canvas.height = height;
     this.polygons = [];
+    this.mouse = {x: 0, y: 0};
+  }
+  
+  
+  mousedown(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const mouseX = event.clientX - this.canvas.offsetLeft;
+    const mouseY = event.clientY - this.canvas.offsetTop;
+    
+    for (let i = 0; i < this.polygons.length; i++) {
+      const polygon = this.polygons[i];
+      for (let j = 0; j < polygon.vertices.length; j++) {
+        const vertex = polygon.vertices[j];
+        if (vertex.isClicked(mouseX, mouseY)) {
+          vertex.dragging = true;
+          console.log(vertex);
+        }
+      }
+    }
   }
   
   click(event) {
-    let polygon = this.getCurrentPolygon();
+    event.preventDefault();
+    event.stopPropagation();
+    const mouseX = event.clientX - this.canvas.offsetLeft;
+    const mouseY = event.clientY - this.canvas.offsetTop;
     
-    let x = event.clientX;
-    let y = event.clientY;
-    console.log('Click en canvas! Coordenadas: ' + x + ', ' + y);
+    // let clickedPolygon;
+    // for (let i = 0; i < this.polygons.length; i++) {
+    //   clickedPolygon = this.polygons[i].isClicked(mouseX, mouseY);
+    // }
+    // if (clickedPolygon) {
+    //   if (clickedPolygon.selectedVertex != null) {
+    //     clickedPolygon.selectedVertex.color = 'blue';
+    //     clickedPolygon.selectedVertex.dragging = true;
+    //     // clickedPolygon.selectedVertex.update(this.mouse.x, this.mouse.y);
     
-    let vertex = new Vertex(x, y, 5);
-    vertex.color = '#FF0000';
-
+    //   }
+    // }
+    
+    
+    const polygon = this.getCurrentPolygon();
+    
+    const vertex = new Vertex(mouseX, mouseY, 5);
+    vertex.color = 'rgba(255, 0, 0, 100)';
+    
     polygon.addVertex(vertex);
+    
+    
     this.draw();
+  }
+  
+  mousemove(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const mouseX = event.clientX - this.canvas.offsetLeft;
+    const mouseY = event.clientY - this.canvas.offsetTop;
+
+    for (let i = 0; i < this.polygons.length; i++) {
+      const polygon = this.polygons[i];
+      for (let j = 0; j < polygon.vertices.length; j++) {
+        const vertex = polygon.vertices[j];
+        if (vertex.dragging) {
+          vertex.update(mouseX, mouseY);
+        }
+      }
+    }
+
+    this.draw();
+  }
+  mouseup(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    for (let i = 0; i < this.polygons.length; i++) {
+      const polygon = this.polygons[i];
+      for (let j = 0; j < polygon.vertices.length; j++) {
+        const vertex = polygon.vertices[j];
+        if (vertex.dragging) {
+          vertex.dragging = false;
+        }
+      }
+    }
+  }
+  
+  clickOnVertex(mouseX, mouseY) {
+    for (let i = 0; i < this.polygons.length; i++) {
+      if (this.polygons[i].isClicked(mouseX, mouseY)) {
+        return true;
+      }
+    }
+    return false;
   }
   clear() {
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -32,30 +110,22 @@ class Canvas {
   }
   getCurrentPolygon() {
     let currentPolygon;
-    if (this.polygons.length == 0) {
-      currentPolygon = new Polygon(this.ctx);
-      this.addPolygon(currentPolygon);
-    } else {
-      this.polygons.forEach(polygon => {
-        if (!polygon.isClosed()) {
-          currentPolygon = polygon;
-        }
-      });
-    }
-    if (currentPolygon == undefined) {
+    this.polygons.forEach((polygon) => {
+      if (!polygon.isClosed()) {
+        currentPolygon = polygon;
+      }
+    });
+    if (currentPolygon == undefined || this.polygons.length == 0) {
       currentPolygon = new Polygon(this.ctx);
       this.addPolygon(currentPolygon);
     }
-
-    console.log(currentPolygon);
-    
     return currentPolygon;
   }
   draw() {
     this.clear();
     if (this.polygons.length > 0) {
-      this.polygons.forEach(polygon => {
-        polygon.draw(this.ctx);
+      this.polygons.forEach((polygon) => {
+        polygon.draw();
       });
     }
   }
--- TP2/EntregaFinal/js/Polygon.js
+++ TP2/EntregaFinal/js/Polygon.js
@@ -7,7 +7,8 @@ class Polygon {
   constructor(context) {
     this.ctx = context;
     this.closed = false;
-    this.centroid = null;
+    this.centroid;
+    this.selectedVertex;
     this.vertices = [];
   }
   
@@ -17,51 +18,76 @@ class Polygon {
   isClosed() {
     return this.closed;
   }
-  draw() {
-    this.vertices.forEach(vertex => {
-      vertex.draw(this.ctx);
-    });
-    
-    // refactor
+  isClicked(mouseX, mouseY) {
     for (let i = 0; i < this.vertices.length; i++) {
-      let vertex = this.vertices[i];
-      let nextVertex = this.vertices[i+1];
-      if (nextVertex != undefined) {
-        this.ctx.strokeStyle = 'yellow';
-        this.ctx.lineWidth = 2;
-        this.ctx.beginPath();
-        this.ctx.moveTo(vertex.x, vertex.y);
-        this.ctx.lineTo(nextVertex.x, nextVertex.y);
-        this.ctx.stroke();
-        this.ctx.closePath();
+      if (this.vertices[i].isClicked(mouseX, mouseY)) {
+        this.selectedVertex = this.vertices[i];
+        return this;
       }
     }
+    return null;
   }
-  close() {
-    let firstVertex = this.vertices[0];
-    let lastVertex = this.vertices[this.vertices.length-1];
+  draw() {
+    if (this.vertices.length > 0) {
+      this.vertices.forEach((vertex) => {
+        vertex.draw(this.ctx);
+      });
+    }
+    if (this.centroid) {
+      this.centroid.draw(this.ctx);
+    }
+    this.drawLines();
+  }
+  drawLines() {
     
-    // refactor
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.vertices[0].x, this.vertices[0].y);
+    for (let i = 1; i < this.vertices.length; i++) {
+      this.ctx.lineTo(this.vertices[i].x, this.vertices[i].y);
+    }
+    if (this.isClosed()) {
+      this.ctx.closePath();
+    }
+    this.ctx.stroke();
+    
+    // for (let i = 0; i < this.vertices.length; i++) {
+      
+    //   let vertex = this.vertices[i];
+    //   let nextVertex = this.vertices[i+1];
+      
+    //   if (nextVertex != undefined) {
+    //     this.line(vertex.x, vertex.y, nextVertex.x, nextVertex.y);
+    //   }
+    // }
+    // if (this.closed) {
+    //   let firstVertex = this.vertices[0];
+    //   let lastVertex = this.vertices[this.vertices.length-1];
+      
+    //   this.line(lastVertex.x, lastVertex.y, firstVertex.x, firstVertex.y);
+    // }
+  }
+  line(x1, y1, x2, y2) {
     this.ctx.strokeStyle = 'yellow';
     this.ctx.beginPath();
-    this.ctx.moveTo(lastVertex.x, lastVertex.y);
-    this.ctx.lineTo(firstVertex.x, firstVertex.y);
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
     this.ctx.stroke();
     this.ctx.closePath();
-
+  }
+  close() {
     this.closed = true;
+    this.setCentroid();
   }
   setCentroid() {
     let sumX = 0; 
     let sumY = 0;
-    this.vertices.forEach(vertex => {
+    this.vertices.forEach((vertex) => {
       sumX += vertex.x;
       sumY += vertex.y;
     });
     let centroidX = sumX / this.vertices.length;
     let centroidY = sumY / this.vertices.length;
-
     this.centroid = new Vertex(centroidX, centroidY, 3.5);
-    this.centroid.color = 'green';
+    this.centroid.color = '#00FF00';
   }
 }
